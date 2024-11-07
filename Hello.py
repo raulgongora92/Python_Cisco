27392d830beaabcc5a38035d5d72a8599f6390b4
Raul Gongora <raulgongora92@gmail.com>
2024-11-07 07:52:39 -0500

    Primer Modulo

---
+++ Hello.py
@@ -0,0 +1,35 @@
+#Función print muestra en pantalla el argumento que s ele introduzca
+print()
+print("Hisssssss...")
+print("-----------------------------")
+
+#caracter  de escape y nueva linea(\n)
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+print("-----------------------------")
+
+#multiples argumentos con print
+print("La Witsi Witsi Araña" , "subió" , "a su telaraña.")
+print("-----------------------------")
+
+#end muestra las 2 lineas en 1 sola seguida
+print("Mi nombre es", "Python.", end=" ")
+print("Monty Python.")
+print("-----------------------------")
+
+#sep cambia el tipo de separación entre argumentos
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+print("-----------------------------")
+
+#mezclar end y sep
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+print("-----------------------------")
+
+#Ejercicio de laboratorio: Salida esperada. Programming***Essentials***in...Python
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+print("-----------------------------")
+
+print()
